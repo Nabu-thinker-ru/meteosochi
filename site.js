789ddb1db596c756b87e6e9b49ca51e363845471
Nabu-thinker-ru <nabu@thinker.ru>
2014-02-07 00:09:43 +0400

    time trouble

---
+++ site.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+
+	//create slide show
+	
+	//load img from timeline
+	$('#currentDay a').click(function(){
+		showImg($(this).attr('href'))
+	})
+	
+	//click nextDay link
+	$('#nextDay a').click(function(){
+		
+	})
+	//click prevDay link
+	$('#prevDay a').click(function(){
+		
+	})
+	
+	//slidedown logo div
+	$('.logo a').click(function(){
+		
+	})
+	
+	
+});
+
+function showImg(dt)
+{
+//fadeout current img
+//fadein img with class="dt"
+}
